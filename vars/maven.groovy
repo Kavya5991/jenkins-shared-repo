aa6390e2c858f9d8ddafb3d27bf28b15c7db2e01
Kavya5991 <130562048+Kavya5991@users.noreply.github.com>
2023-06-30 01:19:01 -0400

    Update maven.groovy

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -29,10 +29,10 @@ def call(String repositoryUrl) {
       stage('Run Container') {
         steps {
           script {
-            sh 'docker run -dt --rm --name Maven_docker_Pipeline_Generic maven_generic'
-            sh 'docker cp pom.xml maven_docker_Pipeline_Generic:/workspace/'
-            sh 'docker exec Maven_docker_Pipeline_Generic mvn -f /workspace/pom.xml --version'
-            sh 'docker exec Maven_docker_Pipeline_Generic mvn -f /workspace/pom.xml clean install'
+            sh 'docker run -dt --rm --name Maven_Docker_Pipeline_Generic maven_generic'
+            sh 'docker cp pom.xml Maven_Docker_Pipeline_Generic:/workspace/'
+            sh 'docker exec Maven_Docker_Pipeline_Generic mvn -f /workspace/pom.xml --version'
+            sh 'docker exec Maven_Docker_Pipeline_Generic mvn -f /workspace/pom.xml clean install'
           }
         }
       }
