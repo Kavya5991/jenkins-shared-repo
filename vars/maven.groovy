7a5af8a3bf93f6b19633be8096c986f26c78f634
Kavya5991 <130562048+Kavya5991@users.noreply.github.com>
2023-06-30 00:10:51 -0400

    Update maven.groovy

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,1 +1,46 @@
+def call(String repositoryUrl){
+pipeline {
+    agent any
+    stages {
+        stage('Create Docker Image') {
+            steps {
+                writeFile file: 'Dockerfile', text: '''
+                    FROM ubuntu:22.04
+                    RUN apt-get update && apt-get install -y wget openjdk-11-jdk
+                    RUN wget -q https://dlcdn.apache.org/maven/maven-3/3.9.3/binaries/apache-maven-3.9.3-bin.tar.gz
+                    RUN tar xf apache-maven-3.9.3-bin.tar.gz -C /opt
+                    RUN rm apache-maven-3.9.3-bin.tar.gz
+                    ENV MAVEN_HOME=/opt/apache-maven-3.9.3
+                    ENV PATH=$MAVEN_HOME/bin:$PATH
+                    RUN mvn --version
+                '''
+            }
+        }
+         stage('Build Docker Image') {
+             steps {
+                     // Build Docker image
+                sh 'docker build -t maven_generic -f Dockerfile .'
+            }
+        }
+      stage('Code Checkout') {
+        steps {
+          git branch:'main',url:"${repositoryUrl}"
+        }
+      }
+        stage('Run Container') {
+            steps {
+                script
+                {
+                    sh 'docker run -dt --rm --name maven_docker_pipeline_generic maven_generic'
+                    sh 'docker exec maven_docker_pipeline_generic mvn -version'
+                    sh 'docker exec maven_docker_pipeline_generic mvn clean install'
+                    
+                  
 
+                }    
+            }
+        }
+       
+    }
+}
+}
