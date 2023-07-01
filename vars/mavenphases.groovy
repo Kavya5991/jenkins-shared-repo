4c1deceabc4f9a10fc507d9bb97ed87aac7e4264
Kavya5991 <130562048+Kavya5991@users.noreply.github.com>
2023-06-30 21:22:55 -0400

    Update mavenphases.groovy

--- vars/mavenphases.groovy
+++ vars/mavenphases.groovy
@@ -11,7 +11,7 @@ def call(String repositoryUrl,String projectKey,String sonarToken,String sonarHo
         steps {
           writeFile file: 'Dockerfile', text: '''
             FROM ubuntu:22.04
-            RUN apt-get update && apt-get install -y wget openjdk-11-jdk
+            RUN apt-get update && apt-get install -y wget openjdk-11-jdk git
             RUN wget -q https://dlcdn.apache.org/maven/maven-3/3.9.3/binaries/apache-maven-3.9.3-bin.tar.gz
             RUN tar xf apache-maven-3.9.3-bin.tar.gz -C /opt
             RUN rm apache-maven-3.9.3-bin.tar.gz
