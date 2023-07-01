cd659a2aca102721d18f8fc48ecd1da5fb59caef
Kavya5991 <130562048+Kavya5991@users.noreply.github.com>
2023-06-30 22:17:47 -0400

    Update mavenphases.groovy

--- vars/mavenphases.groovy
+++ vars/mavenphases.groovy
@@ -26,12 +26,12 @@ def call(String repositoryUrl, String projectKey, String sonarToken, String sona
       }
       stage('Run Container') {
         steps {
-           sh "docker run -dt --rm --name Maven_Phasess -v ~/.ssh:/root/.ssh maven_phases"
+           sh "docker run -dt --rm --name Maven_Phasess -v ~/.ssh:/root/.ssh -v /var/lib/jenkins/workspace/maven/shared-library-demo/maven-phases-sl:/workspace maven_phases"
           sh "docker cp settings.xml  Maven_Phasess:/opt/apache-maven-3.9.3/conf/"
           sh "docker exec Maven_Phasess git config --global user.email 'kavyakolla98@gmail.com'"
           sh "docker exec Maven_Phasess git config --global user.name 'Kavya5991'"
           sh "docker exec Maven_Phasess git remote set-url origin ${repositoryUrl.replace('https://', 'git@')}"
-           //sh "docker exec Maven_Phasess git -C /workspace clone ${repositoryUrl}"
+          sh "docker exec Maven_Phasess git -C /workspace clone ${repositoryUrl}"
           sh "docker exec -e GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=no' Maven_Phasess git push --set-upstream origin master"
         }
         }
